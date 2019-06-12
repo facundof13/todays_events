0bb6135455b9e192ef88468e969c5afba1e9c8f0
Facundo Figueroa <Facundof13@gmail.com>
2019-06-12 01:44:02 -0400

    finally working async and await on google cal api

--- events.js
+++ events.js
@@ -8,25 +8,30 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'token.json';
 let authToken;
 
-fs.readFile('./bin/credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  authorize(JSON.parse(content), setAuthToken);
-});
+startGetCalendarEventsForToday();
 
-function authorize(credentials, callback) {
+
+async function startGetCalendarEventsForToday() {
+  fs.readFile('./bin/credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    authorize(JSON.parse(content), setAuthToken);
+  });
+}
+
+async function authorize(credentials, callback) {
   const {
     client_secret,
     client_id,
     redirect_uris
   } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
+  const oAuth2Client = await new google.auth.OAuth2(
     client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
+  fs.readFile(TOKEN_PATH, async (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    await oAuth2Client.setCredentials(JSON.parse(token));
+    await callback(oAuth2Client);
   });
 }
 
@@ -34,17 +39,18 @@ function authorize(credentials, callback) {
 // *******************
 // 
 //  
-async function setAuthToken(oAuth2Client) {
+async function setAuthToken(oAuth2Client, hours) {
   authToken = oAuth2Client;
   const cals = await getCalendarsAndFilter(authToken);
-  const events = await getEvents(authToken, cals);
+  const events = await getEvents(authToken, cals, hours);
+  console.log(events);
   return events;
 }
 // 
 // 
 // **********************
 
-function getAccessToken(oAuth2Client, callback) {
+async function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'online',
     scope: SCOPES,
@@ -56,9 +62,9 @@ function getAccessToken(oAuth2Client, callback) {
   });
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
+    oAuth2Client.getToken(code, async (err, token) => {
       if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
+      await oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
@@ -77,10 +83,9 @@ async function getCalendarsAndFilter(auth) {
   });
   let allCalendars = [];
   const result = await calendar.calendarList.list();
-  await result.data.items.forEach(function (cal) {
+  result.data.items.forEach(function (cal) {
     if ((cal.summary.indexOf("Contact") < 0)) {
       if ((cal.summary.indexOf("Holiday") < 0)) {
-        5
         allCalendars.push(cal);
       }
     }
@@ -88,27 +93,27 @@ async function getCalendarsAndFilter(auth) {
   return allCalendars;
 }
 
-async function getEvents(auth, calendars) {
+async function getEvents(auth, calendars, hoursLeft) {
   let todayEvents = [];
   const calendar = google.calendar({
     version: 'v3',
     auth
   });
-  calendars.forEach(async function (cal) {
+  for (let i = 0; i < calendars.length; i++) {
     let date = new Date();
     date.setTime(date.getTime() + (25 * 60 * 60 * 1000)); //search 25 hrs from now
     date.toISOString();
     const result = await calendar.events.list({
-      calendarId: cal.id,
+      calendarId: calendars[i].id,
       timeMin: (new Date()).toISOString(),
       timeMax: date,
       orderBy: 'startTime',
       singleEvents: true
     });
     const events = await result.data.items;
-    events.forEach(function (event) {
-      todayEvents.push(cal.summary + ": " + event.summary);
-    });
-  });
+    for (let j = 0; j < events.length; j++) {
+      todayEvents.push(calendars[i].summary + ": " + events[j].summary);
+    }
+  }
   return todayEvents;
 }
